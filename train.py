1984064eb37f4c116c1cc05cb2f0223d2d0ac857
cheng ying <chengying@tezign.com>
2019-07-30 17:03:36 +0800

    add

--- train.py
+++ train.py
@@ -36,13 +36,14 @@ torch.manual_seed(0)
 
 
 class ImagesDS(D.Dataset):
-    def __init__(self, df, img_dir, mode='train', site=1, channels=[1, 2, 3, 4, 5, 6]):
+    def __init__(self, df, img_dir, mode='train', augmentation=False, site=1, channels=[1, 2, 3, 4, 5, 6]):
         self.records = df.to_records(index=False)
         self.channels = channels
         self.site = site
         self.mode = mode
         self.img_dir = img_dir
         self.len = df.shape[0]
+        self.augmentation = augmentation
 
     @staticmethod
     def _load_img_as_tensor(file_name):
@@ -53,8 +54,8 @@ class ImagesDS(D.Dataset):
         experiment, well, plate = self.records[index].experiment, self.records[index].well, self.records[index].plate
         return '/'.join([self.img_dir, self.mode, experiment, f'Plate{plate}', f'{well}_s{self.site}_w{channel}.png'])
 
-    def _transform(self, img, mode):
-        if mode == 'train':
+    def _transform(self, img, augumentation):
+        if augumentation:
             trans = torchvision.transforms.Compose([
                 torchvision.transforms.RandomCrop(224),
                 torchvision.transforms.ToTensor(),
@@ -88,9 +89,9 @@ class ImagesDS(D.Dataset):
         img = rio.load_site_as_rgb(self.mode, experiment, plate, well, np.random.choice([1, 2]))
         img = Image.fromarray(img.astype(np.uint8))
 
-        img = self._transform(img, self.mode)
+        img = self._transform(img, self.augmentation)
 
-        if self.mode == 'train' or self.mode == 'val':
+        if self.mode == 'train':
             return img, int(self.records[index].sirna)
         else:
             return img, self.records[index].id_code
@@ -103,8 +104,8 @@ df = pd.read_csv(path_data + '/train.csv')
 df_train, df_val = train_test_split(df, test_size=0.035, stratify=df.sirna, random_state=42)
 df_test = pd.read_csv(path_data + '/test.csv')
 
-ds = ImagesDS(df_train, path_data, mode='train')
-ds_val = ImagesDS(df_val, path_data, mode='val')
+ds = ImagesDS(df_train, path_data, mode='train', augmentation=True)
+ds_val = ImagesDS(df_val, path_data, mode='train')
 ds_test = ImagesDS(df_test, path_data, mode='test')
 
 classes = 1108
