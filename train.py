6475f67537858b701f5760384774b2e77cd52c54
cheng ying <chengying@tezign.com>
2019-08-06 09:52:30 +0800

    add

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ warnings.filterwarnings('ignore')
 path_data = 'data'
 # device = 'cuda'
 device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
-batch_size = 32
+batch_size = 16
 torch.manual_seed(0)
 use_rgb = False
 model_name = 'resnet_18'
@@ -81,16 +81,16 @@ def compute_and_display_val_metrics(engine):
 
 
 # lr_scheduler = ExponentialLR(optimizer, gamma=0.95)
-lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=5, verbose=True)
+# lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=5, verbose=True)
 
 
-# lr_scheduler = MultiStepLR(optimizer, [15, 50, 100], 0.1)
+lr_scheduler = MultiStepLR(optimizer, [15, 25, 100], 0.1)
 
 
 @trainer.on(Events.EPOCH_COMPLETED)
 def update_lr_scheduler(engine):
-    # lr_scheduler.step()
-    lr_scheduler.step(val_epoch[engine.state.epoch]['accuracy'])
+    lr_scheduler.step()
+    # lr_scheduler.step(val_epoch[engine.state.epoch]['accuracy'])
     # lr_scheduler.step(engine.state.metrics['accuracy'])
     lr = float(optimizer.param_groups[0]['lr'])
     print("Learning rate: {}".format(lr))
