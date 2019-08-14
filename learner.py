f21ef562764e8eab364ae8676f1eec905ca7e2a1
cheng ying <chengying@tezign.com>
2019-08-14 18:28:50 +0800

    add

--- learner.py
+++ learner.py
@@ -134,8 +134,30 @@ def board_val(writer, accuracy, best_threshold, roc_curve_tensor, step):
 
 train_model(model, criterion, optimizer, lr_scheduler, {'train': loader, 'val': val_loader}, writer, 50)
 
-# train_embeddings = []
-# train_labels = []
-#
-# for input, target in loader:
-#
+train_embeddings = []
+train_labels = []
+
+model.eval()
+for i, (input, target) in enumerate(loader):
+    input = input.to(device)
+    target = target.to(device)
+    embedding = model(input, target)
+    train_embeddings.append(embedding)
+    train_labels.append(target)
+
+for i, (input, target) in enumerate(val_loader):
+    input = input.to(device)
+    target = target.to(device)
+    embedding = model(input, target)
+    train_embeddings.append(embedding)
+    train_labels.append(target)
+
+train_embeddings = torch.cat(train_embeddings)
+train_labels = torch.cat(train_labels)
+train_labels = np.array(train_labels.cpu().numpy())
+train_embeddings = np.array(train_embeddings.cpu().numpy())
+
+
+train_csv_path = './data/train.csv'
+df = pd.read_csv(train_csv_path)
+rgb_df.sirna.unique()
