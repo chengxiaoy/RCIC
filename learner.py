8b40fd8cc8902d6d2e80bc969cb2bcb8760c2441
e <chengyingjuly@163.com>
2019-08-22 23:09:12 +0800

    rgb and 6channel mode

--- learner.py
+++ learner.py
@@ -41,7 +41,7 @@ class Config():
     classes = 1108
     pic_size = 448
 
-    stage1_epoch = 100
+    stage1_epoch = 30
     stage2_epoch = 30
 
     stage1_lr = 0.0001
@@ -72,7 +72,7 @@ class Learner:
         return model
 
     def stage_one(self):
-        model = self.build_model()
+        model = self.build_model(weight_path='models/stage1_Aug22_06-57-lr1_0.0001_lr2_0.0001_bs_32_ps_448_backbone_resnet_50_head_arcface_rgb_False.pth')
 
         ds, ds_val, ds_test = get_dataset(self.config.use_rgb, size=self.config.pic_size, pair=False)
         loader = D.DataLoader(ds, batch_size=self.config.train_batch_size, shuffle=True, num_workers=16)
@@ -447,8 +447,8 @@ if __name__ == "__main__":
     learner.confi_evaluate(s1_model)
     # s1_model = learner.build_model(
     #     weight_path='models/stage1_Aug21_12-45_lr1_0.0001_lr2_0.0001_bs_128_ps_448_backbone_resnet_50_head_arcface_rgb_False.pth')
-    #
-    # learner.confi_evaluate(s1_model)
 
-    # s2_model = learner.stage_two(s1_model)
-    # learner.angle_evaluate(s2_model)
+    learner.confi_evaluate(s1_model)
+
+    s2_model = learner.stage_two(s1_model)
+    learner.angle_evaluate(s2_model)
