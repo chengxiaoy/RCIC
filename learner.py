2759230313d73b4cc6b0a7b6c40593499ccb7382
cheng ying <chengying@tezign.com>
2019-08-28 16:40:57 +0800

    add

--- learner.py
+++ learner.py
@@ -41,7 +41,7 @@ class Config():
     classes = 1108
     pic_size = 448
 
-    stage1_epoch = 50
+    stage1_epoch = 60
     stage2_epoch = 60
 
     stage1_lr = 0.0001
@@ -72,7 +72,7 @@ class Learner:
         return model
 
     def stage_one(self):
-        model = self.build_model(weight_path='models/stage1_Aug27_10-21-lr1_0.0001_lr2_0.0001_bs_32_ps_448_backbone_densenet201_head_arcface_rgb_False.pth')
+        model = self.build_model()
 
         ds, ds_val, ds_test = get_dataset(self.config.use_rgb, size=self.config.pic_size, pair=False)
         loader = D.DataLoader(ds, batch_size=self.config.train_batch_size, shuffle=True, num_workers=16)
