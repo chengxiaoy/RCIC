0cf605311100763b17622a8d5846d4e0bbd663a7
cheng ying <chengying@tezign.com>
2019-09-09 11:12:46 +0800

    add

--- learner.py
+++ learner.py
@@ -82,7 +82,7 @@ class Learner:
         model = self.build_model()
 
         ds, ds_val, ds_test = get_dataset(self.config.use_rgb, size=self.config.pic_size, pair=False,
-                                          six_channel=self.config.six_channel_aug)
+                                          six_channel=self.config.six_channel_aug,experment=self.config.experment)
         loader = D.DataLoader(ds, batch_size=self.config.train_batch_size, shuffle=True, num_workers=16)
         val_loader = D.DataLoader(ds_val, batch_size=self.config.val_batch_size, shuffle=False, num_workers=16)
 
