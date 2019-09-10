54c80a0b991e4028b613c39babef4c9afcd067d4
cheng ying <chengying@tezign.com>
2019-09-10 21:34:23 +0800

    add

--- learner.py
+++ learner.py
@@ -79,7 +79,7 @@ class Learner:
         return model
 
     def stage_one(self):
-        model = self.build_model()
+        model = self.build_model(weight_path='models/stage1_Sep10_02-14-lr1_0.0001_lr2_0.0001_bs_32_ps_448_backbone_resnet_50_head_arcface_rgb_False_six_channel_aug_True_experment_all.pth')
         ds, ds_val, ds_test = get_dataset(self.config.use_rgb, size=self.config.pic_size, pair=False,
                                           six_channel=self.config.six_channel_aug,experment=self.config.experment)
         loader = D.DataLoader(ds, batch_size=self.config.train_batch_size, shuffle=True, num_workers=16)
@@ -627,7 +627,7 @@ if __name__ == "__main__":
         # s1_model = learner.stage_one()
         # s1_model = learner.build_model(
         #     weight_path='models/stage1_Sep02_02-39-lr1_0.0001_lr2_0.0001_bs_32_ps_448_backbone_resnet_50_head_arcface_rgb_False_six_channel_aug_False.pth')
-        learner.confi_evaluate(s1_model)
+        # learner.confi_evaluate(s1_model)
 
         s2_model = learner.stage_two(s1_model)
 
